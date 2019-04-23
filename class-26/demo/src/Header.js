583efb3c38b52c52e467ce6b424e078c72d8753a
vladimirsan <vladimirsan@gmail.com>
2019-04-22 21:24:04 -0700

    day 26

---
+++ class-26/demo/src/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+  render(){
+    // Vinicio - render returns JSX
+    return(<h1> :) Welcome to React 401n12 People :)</h1>);
+  }
+}
