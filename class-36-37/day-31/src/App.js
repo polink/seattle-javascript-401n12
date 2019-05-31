2ecb2f60f76a100441eb39250284d8308dc0108b
vladimirsan <vladimirsan@gmail.com>
2019-05-30 19:37:13 -0700

    async actions

--- class-36-37/day-31/src/App.js
+++ class-36-37/day-31/src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import {BrowserRouter, Route} from "react-router-dom";
+import AuthRedirect from "./component/AuthRedirect/AuthRedirect";
+import Dashboard from './component/Dashboard/dashboard';
+import Landing from './component/Landing/landing'
 
 export default class App extends React.Component {
+  // Vinicio - this is in charge of controlling our auth-based routing
+  // Vinicio - the process we use find bugs is to little by little remove all the possibilities
   render() {
     return(
-      <p>Hello World</p>
+      <div>
+        <BrowserRouter>
+          <Route path="*" component={AuthRedirect} />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/login" component={Landing}/>
+          <Route exact path="/signup" component={Landing}/>
+          <Route exact path="/dashboard" compontent={Dashboard}/>
+        </BrowserRouter>
+      </div>
     )
   }
 }
+
 
 
